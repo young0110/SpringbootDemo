a654d7a1df2227de3885db97e98495e5f2711e2d
young <zhangyang@aegis-data.cn>
2019-03-06 17:57:05 +0800

    [+]关于异常的捕获

---
+++ src/main/java/com/spring/demo/controller/advice/ExceptionAction.java
@@ -0,0 +1,30 @@
+package com.spring.demo.controller.advice;
+
+import com.spring.demo.model.common.Msg;
+import com.spring.demo.model.common.ResultData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 统一异常处理
+ * @author zhangyang
+ * @version 0.1
+ * create 2019/1/7
+ */
+@Slf4j
+@RestControllerAdvice
+public class ExceptionAction {
+
+  /**
+   * 通用异常处理，待完善
+   * @param e 未捕获的异常
+   * @return
+   */
+  @ExceptionHandler({Exception.class})
+  public ResultData exception(Exception e) {
+    log.error("未捕获的异常", e);
+    return new ResultData<>(Msg.ERROR);
+  }
+
+}
